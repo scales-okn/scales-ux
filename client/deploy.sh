dbe335d17100c6a8318a7278613364e078df8b41
Scott <scottgdaniel@gmail.com>
2023-01-17 17:22:07 -0600

    automated deployment of new build

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
+
+# react-scripts setup
+
 if [ ! -d node_modules/react-scripts ]
 then
+
+    if [ "$EUID" -ne 0 ]
+    then
+	echo "Root privileges needed for node update; try running with sudo."
+	exit
+    fi
+    
     npm install -g n && n lts && n latest
     npm install --force
 fi
 
+
+# delivery repo setup
+
 if [ ! -d delivery ]
 then
-    mkdir delivery
-    cd delivery
-    git init
-    git	remote add origin root@198.199.81.89:/var/www/html/bare-repo
-    git branch -m master main
+
+    if [ "$EUID" -ne 0 ]
+    then
+	mkdir delivery
+	cd delivery
+	git init
+	git remote add origin root@198.199.81.89:/var/www/html/bare-repo
+	git branch -m master main
+
+    else
+	sudo -u $SUDO_USER mkdir delivery
+	sudo -u $SUDO_USER cd delivery
+	sudo -u $SUDO_USER git init
+	sudo -u $SUDO_USER git remote add origin root@198.199.81.89:/var/www/html/bare-repo
+	sudo -u $SUDO_USER git branch -m master main
+    fi
+    
     cd ..
 fi
+
+
+# actual deployment
 
 npm run build
 rm -r delivery/static
