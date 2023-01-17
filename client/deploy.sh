05da9c5d412ca6ecfa169a3a645bcc020e22c01e
Satyrn Droplet <scottgdaniel@gmail.com>
2023-01-17 22:32:26 +0000

    deploy script fixes

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [ ! -d node_modules/react-scripts ]
+then
+    npm install -g n && n lts && n latest
+    npm install --force
+fi
+
 if [ ! -d delivery ]
 then
     mkdir delivery
     cd delivery
     git init
     git	remote add origin root@198.199.81.89:/var/www/html/bare-repo
+    git branch -m master main
+    cd ..
 fi
 
 npm run build
