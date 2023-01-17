ea40bb37a202a11977ad293fc48ea0466954fa0e
Scott <scottgdaniel@gmail.com>
2023-01-17 14:55:06 -0600

    deploy script now creates delivery folder when nonexistent

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,1 +1,18 @@
-npm run build && rm -r delivery/static && mv -v build/* delivery/ && rm -r build && cd delivery && git add . && git commit -m 'automated deployment of new build' && git push origin main
+#!/bin/bash
+
+if [ ! -d delivery ]
+then
+    mkdir delivery
+    cd delivery
+    git init
+    git	remote add origin root@198.199.81.89:/var/www/html/bare-repo
+fi
+
+npm run build
+rm -r delivery/static
+mv -v build/* delivery/
+rm -r build
+cd delivery
+git add .
+git commit -m 'automated deployment of new build'
+git push origin main
