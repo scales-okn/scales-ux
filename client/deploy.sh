0355b93e3343376a12b7b975e725171652a4d0ba
Scott <scottgdaniel@gmail.com>
2023-01-17 19:22:10 -0600

    hopefully final version of deploy script for now

--- client/deploy.sh
+++ client/deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# general note: the $SUDO_USER logic is surrounding all the commands that *shouldn't* be run as root ($SUDO_USER stores the name of the non-sudo user underneath the original sudo call)
 
-# react-scripts setup
+
+# react-scripts setup (this will probably only run once)
 
 if [ ! -d node_modules/react-scripts ]
 then
@@ -13,11 +15,11 @@ then
     fi
     
     npm install -g n && n lts && n latest
-    npm install --force
+    sudo -u $SUDO_USER npm install --force
 fi
 
 
-# delivery repo setup
+# delivery repo setup (this will probably only run once)
 
 if [ ! -d delivery ]
 then
@@ -28,24 +30,35 @@ then
 	cd delivery
 	git init
 	git remote add origin root@198.199.81.89:/var/www/html/bare-repo
-	git branch -m master main
+	
+	if [ $(git branch --show-current) != 'main' ]
+	then git branch -m master main
+	fi
+	git pull origin main
 
     else
 	sudo -u $SUDO_USER mkdir delivery
-	sudo -u $SUDO_USER cd delivery
-	pwd
+	cd delivery
 	sudo -u $SUDO_USER git init
 	sudo -u $SUDO_USER git remote add origin root@198.199.81.89:/var/www/html/bare-repo
-	sudo -u $SUDO_USER git branch -m master main
+	
+	if [ $(git branch --show-current) != 'main' ]	  
+	then sudo -u $SUDO_USER git branch -m master main
+	fi
+	sudo -u $SUDO_USER git pull origin main
     fi
-    
+
     cd ..
 fi
 
 
-# actual deployment
+# actual deployment (most of the time, this is the only block that will run)
 
-npm run build
+if [ "$EUID" -ne 0 ]
+then npm run build
+else sudo -u $SUDO_USER npm run build
+fi
+
 rm -r delivery/static
 mv -v build/* delivery/
 rm -r build
