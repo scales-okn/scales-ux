df72c2ed653cbfe765df754011c1f6c45f93aeab
Scott <scottgdaniel@gmail.com>
2023-04-24 16:43:00 -0500

    fix urls generated for multi-filter searches

--- server/src/proxy/index.ts
+++ server/src/proxy/index.ts
@@ -5,14 +5,33 @@ const router = express.Router();
 
 router.all(
   "*",
-  requestProxy({
-    url: `${process.env.CORE_API_ENDPOINT}/*`,
-    timeout: 30000,
-    headers: {
-      "Allow-Control-Allow-Origin": "*",
-      "x-api-key": process.env.CORE_API_KEY,
-    },
-  })
+  (req, res, next) => {
+
+    // whoever's fault it is that multi-value query params were getting lost en route to the API...I want a word with them >:(
+    var rewrittenParams = {};
+    var urlParamStrings = req.url.split("?").slice(-1)[0].split("&");
+    for (const paramString of urlParamStrings) {
+      if (paramString.includes("[]=")) { // this must be an array-style param, so we'll manually pass it via requestProxy's "query" argument
+        var [param, value] = paramString.split("[]=");
+        if (!(param in rewrittenParams)) {
+          rewrittenParams[param] = [decodeURI(value)];
+        }
+        else {
+          rewrittenParams[param].push(decodeURI(value));
+        }
+      }
+    }
+
+    return requestProxy({
+      url: `${process.env.CORE_API_ENDPOINT}/*`,
+      query: rewrittenParams,
+      timeout: 30000,
+      headers: {
+        "Allow-Control-Allow-Origin": "*",
+        "x-api-key": process.env.CORE_API_KEY,
+      },
+    })(req, res, next)
+  }
 );
 
 export default router;
