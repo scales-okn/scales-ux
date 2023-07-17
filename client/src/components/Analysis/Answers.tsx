b5dc8d88e8fef43f06563e24ae11d54d2505da49
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-07-17 08:54:41 -0700

    remove dots without hover

--- client/src/components/Analysis/Answers.tsx
+++ client/src/components/Analysis/Answers.tsx
@@ -1,5 +1,17 @@
 import React, { memo, useEffect, useState } from "react";
-import { BarChart, Bar, Label, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } from "recharts";
+import {
+  BarChart,
+  Bar,
+  Label,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+  LineChart,
+  Line,
+} from "recharts";
 import { Col } from "react-bootstrap";
 import Loader from "components/Loader";
 import { usePanel } from "store/panels";
@@ -7,7 +19,14 @@ import dayjs from "dayjs";
 import { isEmpty } from "lodash";
 import renderHTML from "helpers/renderHTML";
 
-const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => {
+const Answers = ({
+  panelId,
+  data,
+  satyrn,
+  loadingAnswers,
+  statement,
+  plan,
+}) => {
   const [answerType, setAnswerType] = useState("bar");
   const [answer, setAnswer] = useState(null);
   const { filters } = usePanel(panelId);
@@ -33,11 +52,15 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
             if (!type || !value) return;
 
             if (type === "dateFiled") {
-              value = `[${value?.map((date) => dayjs(date).format("YYYY-M-DD"))}]`;
+              value = `[${value?.map((date) =>
+                dayjs(date).format("YYYY-M-DD"),
+              )}]`;
             }
 
             if (acc[type]) {
-              acc[type] = formatString(acc[type].concat(` ${connector} ${value}`));
+              acc[type] = formatString(
+                acc[type].concat(` ${connector} ${value}`),
+              );
             } else {
               acc[type] = formatString(value);
             }
@@ -59,7 +82,9 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
         }, {})
       : {};
 
-    setAnswer(satyrn.responseManager.generate(formattedFilters, statement?.plan, data));
+    setAnswer(
+      satyrn.responseManager.generate(formattedFilters, statement?.plan, data),
+    );
   }, [data, plan, satyrn, statement, filters]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // for multiline, assumes the order of unit names is line label, x, y
@@ -78,7 +103,9 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
   const formatLineData = (result) => {
     return {
       name: result?.[1] == -1 ? "criminal" : String(result?.[0]),
-      [xUnits]: /^[a-zA-Z ]+$/.test(result?.[0]) ? result?.[0] : parseInt(result?.[0]),
+      [xUnits]: /^[a-zA-Z ]+$/.test(result?.[0])
+        ? result?.[0]
+        : parseInt(result?.[0]),
       [yUnits]: parseInt(result?.[1]),
     };
   };
@@ -95,14 +122,26 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
   const formatMultilineData = (result, label) => {
     return {
       name: result?.[1],
-      [xUnits]: /^[a-zA-Z ]+$/.test(result?.[0]) ? result?.[0] : result?.[0].includes("/") ? convertDate(result?.[0]) : parseInt(result?.[0]),
+      [xUnits]: /^[a-zA-Z ]+$/.test(result?.[0])
+        ? result?.[0]
+        : result?.[0].includes("/")
+        ? convertDate(result?.[0])
+        : parseInt(result?.[0]),
       [label]: parseInt(result?.[1]),
     };
   };
 
   const getColor = () => {
     // from stackoverflow.com/questions/43193341/how-to-generate-random-pastel-or-brighter-color-in-javascript
-    return "hsl(" + 360 * Math.random() + "," + (20 + 75 * Math.random()) + "%," + (10 + 60 * Math.random()) + "%)";
+    return (
+      "hsl(" +
+      360 * Math.random() +
+      "," +
+      (20 + 75 * Math.random()) +
+      "%," +
+      (10 + 60 * Math.random()) +
+      "%)"
+    );
   };
 
   return (
@@ -134,7 +173,10 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="name" />
                   <YAxis
-                    domain={[0, Math.max(...data.results.map((i) => parseInt(i[1])))]}
+                    domain={[
+                      0,
+                      Math.max(...data.results.map((i) => parseInt(i[1]))),
+                    ]}
                     tickFormatter={(value) =>
                       new Intl.NumberFormat("en-US", {
                         notation: "compact",
@@ -142,7 +184,11 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                       }).format(value)
                     }
                   />
-                  <Tooltip formatter={(value) => new Intl.NumberFormat("en").format(value)} />
+                  <Tooltip
+                    formatter={(value) =>
+                      new Intl.NumberFormat("en").format(value)
+                    }
+                  />
                   <Legend />
                   <Bar dataKey={yUnits} fill="#82ca9d" />
                 </BarChart>
@@ -184,9 +230,19 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                         value={yUnits}
                       />
                     </YAxis>
-                    <Tooltip formatter={(value) => new Intl.NumberFormat("en").format(value)} />
+                    <Tooltip
+                      formatter={(value) =>
+                        new Intl.NumberFormat("en").format(value)
+                      }
+                    />
                     <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                    <Line type="monotone" dataKey={yUnits} stroke="#82ca9d" />
+                    <Line
+                      type="monotone"
+                      dataKey={yUnits}
+                      stroke="#82ca9d"
+                      dot={false}
+                      activeDot={{ stroke: "white", strokeWidth: 2, r: 5 }}
+                    />
                     {/* the below was plotting x-values (e.g. years) as y-values; not sure what the intended outcome was */}
                     {/* <Line
                     type="monotone"
@@ -201,7 +257,30 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                 <ResponsiveContainer width="100%" height="80%">
                   <LineChart>
                     {/* not sure why all these props are needed when plotting multiple lines despite not being needed above */}
-                    <XAxis height={80} scale="auto" dataKey={xUnits} interval={0} type={/^[a-zA-Z ]+$/.test(data.results?.[0]?.series?.[0][0]) ? "category" : "number"} /* hack */ domain={/^[a-zA-Z ]+$/.test(data.results?.[0]?.series?.[0][0]) ? undefined : ["dataMin", "dataMax"]} /* hack */ allowDuplicatedCategory={false} tickFormatter={(value) => (data.results?.[0]?.series?.[0][0]?.includes("/") /* hack */ ? dayjs(value).format("M/YYYY") : value)}>
+                    <XAxis
+                      height={80}
+                      scale="auto"
+                      dataKey={xUnits}
+                      interval={0}
+                      type={
+                        /^[a-zA-Z ]+$/.test(data.results?.[0]?.series?.[0][0])
+                          ? "category"
+                          : "number"
+                      }
+                      /* hack */ domain={
+                        /^[a-zA-Z ]+$/.test(data.results?.[0]?.series?.[0][0])
+                          ? undefined
+                          : ["dataMin", "dataMax"]
+                      }
+                      /* hack */ allowDuplicatedCategory={false}
+                      tickFormatter={(value) =>
+                        data.results?.[0]?.series?.[0][0]?.includes(
+                          "/",
+                        ) /* hack */
+                          ? dayjs(value).format("M/YYYY")
+                          : value
+                      }
+                    >
                       <Label
                         style={{
                           textTransform: "capitalize",
@@ -230,7 +309,17 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                         value={yUnits}
                       />
                     </YAxis>
-                    <Tooltip formatter={(value) => new Intl.NumberFormat("en").format(value)} labelFormatter={(value) => (data.results?.[0]?.series?.[0][0]?.includes("/") ? dayjs(value).format("M/YYYY") : value)} /> {/* hack */}
+                    <Tooltip
+                      formatter={(value) =>
+                        new Intl.NumberFormat("en").format(value)
+                      }
+                      labelFormatter={(value) =>
+                        data.results?.[0]?.series?.[0][0]?.includes("/")
+                          ? dayjs(value).format("M/YYYY")
+                          : value
+                      }
+                    />{" "}
+                    {/* hack */}
                     <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
                     {data.results?.map((line_data) => {
                       if (line_data.label == -1) {
@@ -242,13 +331,17 @@ const Answers = ({ panelId, data, satyrn, loadingAnswers, statement, plan }) => 
                       return (
                         <Line
                           key={line_data.label}
-                          dot={!data.results?.[0]?.series?.[0][0]?.includes("/")} /* hack */
+                          // dot={
+                          //   !data.results?.[0]?.series?.[0][0]?.includes("/")
+                          // } /* hack */
                           data={line_data.series?.map((result) => {
                             return formatMultilineData(result, line_data.label);
                           })}
                           type="monotone"
                           dataKey={line_data.label}
                           stroke={getColor()}
+                          dot={false}
+                          activeDot={{ stroke: "white", strokeWidth: 2, r: 5 }}
                         />
                       );
                     })}
