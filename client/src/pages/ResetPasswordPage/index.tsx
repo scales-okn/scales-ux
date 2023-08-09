32d3393b8a688b677cc1125fbb98bfc673b1c230
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-08-09 11:39:23 -0700

    reskin password reset page

--- client/src/pages/ResetPasswordPage/index.tsx
+++ client/src/pages/ResetPasswordPage/index.tsx
@@ -6,8 +6,7 @@ import * as yup from "yup";
 import { useNotify } from "components/Notifications";
 
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBalanceScale } from "@fortawesome/free-solid-svg-icons";
+import PageLayout from "../../components/PageLayout";
 
 interface ResetPasswordFields {
   password: string;
@@ -75,58 +74,59 @@ const ResetPassword: FunctionComponent = () => {
   });
 
   return (
-    <Container className="h-100">
-      <Row className="h-100 justify-content-center align-items-center text-center">
-        <Col md="5">
-          <Form noValidate onSubmit={formik.handleSubmit}>
-            <FontAwesomeIcon icon={faBalanceScale} size="3x" className="mb-4" />
-            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-            <div className="form-floating">
-              <Form.Control
-                type="password"
-                name="password"
-                placeholder="Password"
-                className="rounded-0 rounded-top"
-                value={formik.values.password}
-                onChange={formik.handleChange}
-                isInvalid={
-                  formik.touched.password && Boolean(formik.errors?.password)
-                }
-              />
-              <Form.Label>Password</Form.Label>
-            </div>
-            <div className="form-floating mb-3">
-              <Form.Control
-                type="password"
-                name="repassword"
-                placeholder="Confirm Password"
-                className="rounded-0 rounded-bottom border-top-0"
-                value={formik.values.repassword}
-                onChange={formik.handleChange}
-                isInvalid={
-                  formik.touched.repassword &&
-                  Boolean(formik.errors?.repassword)
-                }
-              />
-              <Form.Label>Confirm Password</Form.Label>
-            </div>
-            <Button
-              variant="primary"
-              type="submit"
-              className="w-100 mb-3 text-white"
-              size="lg"
-            >
-              Reset Password
-            </Button>
-            <Row className="mb-5">
-              <Col className="text-end">
-                <a href="/sign-in" className="small">{`Sign in`}</a>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <PageLayout>
+      <Container className="h-100">
+        <Row className="h-100 justify-content-center align-items-center text-center">
+          <Col md="5">
+            <Form noValidate onSubmit={formik.handleSubmit}>
+              <h1 className="h3 mb-5 mt-4 fw-normal">Set a New Password</h1>
+              <div className="form-floating">
+                <Form.Control
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="rounded-0 rounded-top"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  isInvalid={
+                    formik.touched.password && Boolean(formik.errors?.password)
+                  }
+                  style={{ marginBottom: "12px" }}
+                />
+                <Form.Label>Password</Form.Label>
+              </div>
+              <div className="form-floating mb-3">
+                <Form.Control
+                  type="password"
+                  name="repassword"
+                  placeholder="Confirm Password"
+                  className="rounded-0 rounded-bottom border-top-0"
+                  value={formik.values.repassword}
+                  onChange={formik.handleChange}
+                  isInvalid={
+                    formik.touched.repassword &&
+                    Boolean(formik.errors?.repassword)
+                  }
+                />
+                <Form.Label>Confirm Password</Form.Label>
+              </div>
+              <Button
+                type="submit"
+                className="w-100 mb-3 text-white"
+                size="lg"
+                style={{
+                  background: "var(--main-purple-light)",
+                  border: "none",
+                  marginTop: "20px",
+                }}
+              >
+                Reset Password
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </PageLayout>
   );
 };
 
