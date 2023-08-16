66f9f0884d074f618cc9eeff1130ee1fe417f442
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-08-16 09:09:40 -0700

    add signin forwarding

--- client/src/pages/EmailVerificationPage/index.tsx
+++ client/src/pages/EmailVerificationPage/index.tsx
@@ -1,15 +1,30 @@
-import React, { FunctionComponent, useState } from "react";
-import { useEffect, useCallback } from "react";
-import { useParams } from "react-router-dom";
-// import { useNotify } from "../../components/Notifications";
-import { Container, Row, Col } from "react-bootstrap";
+import React, { FunctionComponent, useState, useCallback } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+
+import { useEffectOnce } from "react-use";
 import Loader from "components/Loader";
-import renderHTML from "helpers/renderHTML";
+import { styles } from "./styles";
 
 const EmailVerificationPage: FunctionComponent = () => {
   const { token } = useParams();
+  const navigate = useNavigate();
+
   const [isLoading, setIsLoading] = useState(true);
-  const [message, setMessage] = useState("");
+  const [status, setStatus] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(5);
+
+  const setTimer = () => {
+    let time = 5;
+    const timer = setInterval(() => {
+      if (time > 1) {
+        setTimeLeft((prev) => prev - 1);
+        time--;
+      } else {
+        clearInterval(timer);
+        navigate("/sign-in");
+      }
+    }, 1000);
+  };
 
   const verifyEmail = useCallback(async () => {
     try {
@@ -22,36 +37,67 @@ const EmailVerificationPage: FunctionComponent = () => {
           token,
         }),
       });
-      const { message } = await response.json();
+      const { code } = await response.json();
+
+      if (code === 200) {
+        setTimer();
+      }
+
       setIsLoading(false);
-      setMessage(message);
-      // TODO: forward to sign in page with success message
+      setStatus(code);
     } catch (error) {
       console.warn(error); // eslint-disable-line no-console
     }
   }, [token]);
 
-  useEffect(() => {
+  useEffectOnce(() => {
     verifyEmail();
-  }, [verifyEmail]);
+  });
+
+  const messages = {
+    400: (
+      <div>
+        <p>Invalid email verification code, or email is already verified.</p>
+        <p>
+          Please <Link to="/">sign in</Link> to continue or contact{" "}
+          <Link
+            to="#"
+            onClick={(e) => {
+              window.location.href = "mailto:admin@scales-okn.org";
+              e.preventDefault();
+            }}
+          >
+            admin@scales-okn.org
+          </Link>{" "}
+          for support.
+        </p>
+      </div>
+    ),
+    200: (
+      <div>
+        <p>
+          Your account has been approved and you can now access Satyrn! You will
+          be forwarded to the login page in <strong>{timeLeft}</strong> seconds.
+        </p>
+      </div>
+    ),
+  };
 
   return (
-    <Container className="h-100">
-      <Row className="h-100 justify-content-center align-items-center text-center">
-        <Col md="9">
-          <img
-            src="https://scales-okn.org/wp-content/uploads/2021/02/PreLoader.png"
-            height="50px"
-            width="50px"
-            alt="scales-logo"
-          />
-          <h1 className="h3 mb-5 fw-normal">Email Verification</h1>
-          <Loader animation="border" isVisible={isLoading}>
-            <p>{renderHTML(message)}</p>
-          </Loader>
-        </Col>
-      </Row>
-    </Container>
+    <div className={styles}>
+      <div className="container">
+        <img
+          src="https://scales-okn.org/wp-content/uploads/2021/02/PreLoader.png"
+          height="120px"
+          width="120px"
+          alt="scales-logo"
+        />
+        <h1 className="title">Welcome to SCALES!</h1>
+        <Loader animation="border" isVisible={isLoading}>
+          <p>{messages[status]}</p>
+        </Loader>
+      </div>
+    </div>
   );
 };
 
