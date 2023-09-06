cdbc56b1252763080b992b90dc3c1ef21fecd4b0
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-09-06 08:43:06 -0700

    lint

--- client/src/store/helpTexts.ts
+++ client/src/store/helpTexts.ts
@@ -2,7 +2,6 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { RootState, AppDispatch } from "store";
 import { authSelector } from "store/auth";
 import { useDispatch, useSelector } from "react-redux";
-import { notify } from "reapop";
 import { authorizationHeader } from "utils";
 
 type InitialStateT = {
