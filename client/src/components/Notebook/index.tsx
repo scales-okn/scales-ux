d310d0e709bba49dd4043be4764496b20ddd91cf
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-09-28 13:10:30 -0700

    lint

--- client/src/components/Notebook/index.tsx
+++ client/src/components/Notebook/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate, useLocation } from "react-router-dom";
 import { useEffectOnce } from "react-use";
 
