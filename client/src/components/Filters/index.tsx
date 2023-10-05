697e41ea5ddd734b9c761b1efd03fc880fe5aac2
Ben Kiggen <benjamin.kiggen@eyecuelab.com>
2023-10-05 07:27:45 -0700

    persist filters

--- client/src/components/Filters/index.tsx
+++ client/src/components/Filters/index.tsx
@@ -15,7 +15,12 @@ type FiltersProps = {
 };
 
 const Filters = ({ panelId }: FiltersProps) => {
-  const { filters = [], setPanelFilters, getPanelResults } = usePanel(panelId);
+  const {
+    filters = [],
+    setPanelFilters,
+    getPanelResults,
+    updatePanel,
+  } = usePanel(panelId);
 
   const filterElements = useMemo(() => {
     return (
@@ -24,6 +29,11 @@ const Filters = ({ panelId }: FiltersProps) => {
       }) || []
     );
   }, [filters]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const handleUpdateResults = () => {
+    getPanelResults();
+    updatePanel({ filters });
+  };
 
   return (
     <div className={`filter-container ${filterContainerStyles}`}>
@@ -53,7 +63,7 @@ const Filters = ({ panelId }: FiltersProps) => {
           {filters?.length ? (
             <Button
               variant="contained"
-              onClick={() => getPanelResults(filters)}
+              onClick={handleUpdateResults}
               style={{ position: "absolute", right: "24px", top: "32px" }}
             >
               Update Results
